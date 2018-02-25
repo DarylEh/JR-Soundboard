c8ccb4ff903712eef0dfed9474137c747433e039
Daryl Amodan <darylamodan@gmail.com>
2018-02-24 23:48:37 -0500

    added onClick function

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,43 @@
-const ohHey = "Hello World";
 
-console.log(ohHeyyuppp);
+function onKeyDown(e){
+    //grabbing keyboard keycodes
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) {
+        return;
+    }
+    key.classList.add('playing');
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    
+}
+function onClick(e) {
+    //takes in the nodelist and grabs the keycode via this.dataset.key
+    const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
+    const key = document.querySelector(`div[data-key="${this.dataset.key}"]`);
+    playSound(audio, key);
+}
+function playSound(audio, key) {
+    if (!audio) return;
+    //adding .playing class for transition and animations
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+} 
+
+function removeTransition(e) {
+    if (e.propertyName !== 'box-shadow') return;
+    e.target.classList.remove('playing');
+}
+
+// variable grabs all divs labeled .key
+const keys = document.querySelectorAll('.key');
+
+//returns a node list and goes thru each item to listen for a click event and runs onClick function
+keys.forEach(key => key.addEventListener('click', onClick));
+
+//returns a node list and goes thru each item to listen for a transitionend event and runs
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+//listens for a keydown event and runs onKeyDown function
+window.addEventListener('keydown', onKeyDown);
